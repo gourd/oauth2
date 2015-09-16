a3d6ff5a7316bb119a77b1107c076f866fb60125
Koala Yeung <koalay@gmail.com>
2015-09-16 15:49:28 +0800

    Added route message
    RoutePat logs to show the resulting endpoints path.

--- route.go
+++ route.go
@@ -2,10 +2,18 @@ package oauth2
 
 import (
 	"github.com/gorilla/pat"
+
+	"log"
 )
 
 // RoutePat adds manager's endpoint to a pat router
 func RoutePat(rtr *pat.Router, base string, ep *Endpoints) {
+
+	// tell user where the endpoints are
+	log.Printf("OAuth2 endpoints"+
+		"\nauthorize endpoint: %s/authorize"+
+		"\ntoken endpoint:     %s/token",
+		base, base)
 
 	// bind handler with pat
 	rtr.Get(base+"/authorize", ep.Auth)
